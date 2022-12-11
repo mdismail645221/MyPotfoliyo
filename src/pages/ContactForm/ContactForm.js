4f5e912b48c6aa1a4dcdfa9d9726312463f2686b
mdismail645221 <mdismail645221@gmail.com>
2022-12-11 10:46:47 +0600

    emailsending added

--- src/pages/ContactForm/ContactForm.js
+++ src/pages/ContactForm/ContactForm.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
 
-    const [name, setName] = useState(null)
-    const [email, setEmail]= useState(null)
-    const [subject, setSubject]= useState(null)
-    const [message, setMessage]= useState(null)
+    // const [name, setName] = useState(null)
+    // const [email, setEmail]= useState(null)
+    // const [subject, setSubject]= useState(null)
+    // const [message, setMessage]= useState(null)
+    const form = useRef();
 
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        const userInfo = {
-            name, 
-            email,
-            subject,
-            message
-        }
-        console.log(userInfo)
+
+        console.log("form", form.current)
+       
+
+        // emailjs.sendForm('service_x4nh5tp', 'template_io7ln3i', form.current, 'Etr_lPr_3CDEGl3xw')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //         console.log("meassage send")
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
     }
 
 
@@ -26,27 +32,27 @@ const ContactForm = () => {
                 <h3 className='text-[#4868ff] text-4xl font-bold mb-12'>Contact Me</h3>
             </div>
             {/* contact form section start */}
-            <form onSubmit={handleSubmit} className="w-96 mx-auto">
+            <form ref={form} onSubmit={handleSubmit} className="w-96 mx-auto">
                 <div className='mb-3'>
                     <label className='text-white font-semibold'>Name:</label><br />
-                    <input onChange={(e) => setName(e.target.value)} type="text" name='name' placeholder="enter your Name" className='px-4 py-3 rounded-lg shadow-lg w-full' required/>
+                    <input type="text" name="to_name"  placeholder="enter your Name" className='px-4 py-3 rounded-lg shadow-lg w-full' required/>
                 </div>
                 {/* email fild */}
                 <div className='mb-3'>
                     <label className='text-white font-semibold'>Email:</label>
-                    <input onChange={(e) => setEmail(e.target.value)} type="email" name='email' placeholder="enter your Name" className='px-4 py-3 rounded-lg shadow-lg w-full' required />
+                    <input type="email" name="from_email" placeholder="enter your Name" className='px-4 py-3 rounded-lg shadow-lg w-full' required />
                 </div>
                 {/* email Subject */}
-                <div className='mb-3'>
+                {/* <div className='mb-3'>
                     <label className='text-white font-semibold'>Subject:</label>
                     <input onChange={(e) => setSubject(e.target.value)}   type="text" name='subject' placeholder="subject" className='px-4 py-3 rounded-lg shadow-lg w-full' required />
-                </div>
+                </div> */}
                 {/* email message */}
                 <div className='mb-3'>
                     <label className='text-white font-semibold'>Message:</label>
-                    <textarea onChange={(e) => setMessage(e.target.value)}  type="text" name='message' placeholder="message" className='px-4 py-3 rounded-lg shadow-lg w-full'required />
+                    <textarea type="text" name="message" placeholder="message" className='px-4 py-3 rounded-lg shadow-lg w-full'required />
                 </div>
-                <button type='submit' value="submit" className='common-btn'>Submit</button>
+                <input type="submit" value="Send" className='common-btn'></input>
             </form>
         </section>
     );
