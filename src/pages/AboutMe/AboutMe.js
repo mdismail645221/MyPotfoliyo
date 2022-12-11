87014ec6479167d05ae8a3e45d37f359d629dd8c
mdismail645221 <mdismail645221@gmail.com>
2022-12-11 12:49:26 +0600

    about me section added

---
+++ src/pages/AboutMe/AboutMe.js
@@ -0,0 +1,22 @@
+ import React from 'react';
+ 
+ const AboutMe = () => {
+    return (
+        <section className='container mx-auto mb-32'>
+            <div className='abouteMe-title text-center text-4xl font-bold text-[#4868ff]'>
+                <h3>About Me</h3>
+            </div>
+            {/* --------about me hero start ---------- */}
+            <div className='grid gap-5 grid-cols-1 md:grid-cols-2'>
+                <div>
+                    <a href='#'><img src="" alt="me" /></a>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </section>
+    );
+ };
+ 
+ export default AboutMe;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import About from '../About/About';
+import AboutMe from '../AboutMe/AboutMe';
 import ContactForm from '../ContactForm/ContactForm';
 import MyProjects from '../MyProjects/MyProjects';
 
@@ -10,6 +11,7 @@ const Home = () => {
             <Header></Header>
             <About></About>
             <MyProjects></MyProjects>
+            <AboutMe></AboutMe>
             <ContactForm/>
         </div>
     );
